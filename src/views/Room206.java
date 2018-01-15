ff6e48fe836e9ac5ac867ddbb5203fc1d0908f29
Olive <Olive@10.0.0.244>
2018-01-15 16:36:25 -0500

    Original version of room 206

--- src/views/Room206.java
+++ src/views/Room206.java
@@ -5,6 +5,7 @@
  */
 package views;
 
+import static com.sun.javafx.application.PlatformImpl.exit;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import javax.swing.ImageIcon;
@@ -14,7 +15,10 @@ import models.CharacterMovement;
 import models.Customer;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import static java.lang.System.exit;
+import static javafx.application.Platform.exit;
 import javax.swing.JOptionPane;
+import static jdk.nashorn.internal.objects.Global.exit;
 
 /**
  *
@@ -230,207 +234,10 @@ public class Room206 extends JPanel {
 
         g.drawImage(new ImageIcon("room206.png").getImage(), 0, 0, null);
         g.drawImage(new ImageIcon(characterMovement.getAnimation()).getImage(), student.x, student.y, null);
-
+        if(student.x >= 80&&student.x<=90 && student.y >= 500&&student.y<=550){
+            JOptionPane.showMessageDialog(temp,"Key is found,room is now unlocked");
+        }
+    
     }
 
 }
-
-
-    
-    
-//    int max = 3;
-//    int[] [] map,maptmp;
-//    int manX, manY,boxnum;
-//    Image[] myImage;
-//    Readmap map1;
-//    Readmap maptmp1;
-//    int len = 30;
-//    int level = 1;
-//    Stack mystack = new Stack();
-//    
-//    public Room206(Customer inf_Student, CharacterMovement inf_charMovement) {
-//        super();
-//        student = inf_Student;
-//        characterMovement = inf_charMovement;
-//        setSize(800, 600);
-//        setLayout(null);
-//        add(temp);
-//        temp.setBounds(200, 200, 200, 200);
-//        init();
-//        placeStations();
-//        this.addKeyListener(characterMovement);
-//
-//        this.addMouseMotionListener(new MouseAdapter() {
-//            @Override
-//            public void mouseMoved(MouseEvent e)
-//            {
-////                System.out.println(e.getPoint());
-//                temp.setText(e.getPoint().toString());
-//            }
-//        });
-//        this.setFocusable(true);
-//    }
-//    Room206(){
-//        setSize(600,620);
-//        setBounds(15,50,600,600);
-//        setBackground(Color.white);
-//        addKeyListener(this);
-//       //  myImage = new Image[10];
-//       // for(int i=0; i<10; i++){
-//       //     myImage[i] = Toolkit.getDefaultToolkit().getImage("pic\\"+i+".gif");
-//       // }
-//        setVisible(true);
-//        this.pushboxes(this.level);
-//        this.requestFocus();
-//        validate();
-//    }
-//    
-//    void pushboxes(int i){
-//        map1 = new Readmap(i);
-//        maptmp1 = new Readmap(i);
-//        map = map1.getmap();
-//        manX = map1.getmanX();
-//        manY = map1.getmanY();
-//        maptmp = maptmp1.getmap();
-//        repaint();
-//        
-//    }
-//    
-//   
-//    
-//    public void paint(Graphics g){
-//         super.paint(g);
-//
-//        g.drawImage(new ImageIcon("new_Room.png").getImage(), 0, 0, null);
-//        }
-//       
-//    
-//    @Override
-//    public void keyTyped(KeyEvent ke) {
-//    }
-//
-//    @Override
-//    public void keyPressed(KeyEvent ke) {
-//        if (ke.getKeyCode() == KeyEvent.VK_UP){
-//            moverup();
-//        }
-//        if (ke.getKeyCode() == KeyEvent.VK_DOWN){
-//            movedown();
-//        }
-//        if(ke.getKeyCode() == KeyEvent.VK_LEFT){
-//            moveleft();
-//        }
-//        if(ke.getKeyCode() == KeyEvent.VK_RIGHT){
-//            moveright();
-//            
-//        }
-//        if(unlock()){
-//           
-//                JOptionPane.showMessageDialog(this, "The Door Opened!");
-//                mystack.removeAllElements();
-//        }
-//    }
-//
-//    @Override
-//    public void keyReleased(KeyEvent ke) {
-//    }
-//    
-//    boolean isMystackEmpty(){
-//        return mystack.isEmpty();
-//    }
-//    void remove(){
-//        mystack.removeAllElements();
-//    }
-//    int back() {
-//        return (int) mystack.pop();
-//    }
-//
-//    private void moverup() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    private void movedown() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    private void moveleft() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    private void moveright() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    private boolean unlock() {
-//        boolean num = false;
-//        out: for(int i = 0; i<20; i++)
-//                for (int j = 0; j<20; j++){
-//            if(maptmp [i][j] == 4 || maptmp[i][j] ==9)
-//                    if (map[i][j] ==9)
-//                        num = true;
-//                    else {
-//                        num = false;
-//                        break out;
-//                            }
-//            }
-//        return num;            
-//    }
-//                
-//
-//}
-//    
-//
-//
-//class Readmap{
-//    private int level , mx, my;
-//    private int [] [] mymap = new int[20][20];
-//    FileReader r;
-//    BufferedReader br;
-//    String bb="";
-//    int []x;int c = 0;
-//    Readmap(int k){
-//        level = k;
-//        String s;
-//        try {
-//            File f = new File("maps\\"+level+".map");
-//            r = new FileReader (f);
-//            br = new BufferedReader(r);
-//        }
-//        catch (IOException e){
-//            System.out.println(e);
-//        }
-//    
-//    try{
-//     while ((s=br.readLine())!=null){
-//	bb=bb+s;
-//        }
-//    }
-//    catch (IOException g){
-//	System.out.println(g);
-//	}
-//    
-//    byte [] d = bb.getBytes();
-//    int len = bb.length();
-//    int [] x = new int[len];
-//    for (int i=0; i<bb.length();i++)x[i] = d[i]-48;
-//    for (int i=0; i<20; i++){
-//        for(int j = 0; i <20; j++){
-//            mymap[i][j]= x[c];
-//            if(mymap [i][j] == 5){
-//                mx = j;
-//                my = i;
-//            }
-//            c++;
-//        }
-//    }
-//    }
-//    int [][] getmap(){
-//        return mymap;
-//    }
-//    int getmanX(){
-//        return mx;
-//    }
-//    int getmanY(){
-//        return my;
-//    }
-//}
